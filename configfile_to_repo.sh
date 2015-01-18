84d616bab47b14c3caab462d5743dd4501d87a3a
Rik <rik.roverts@comcordis.com>
2015-01-18 17:34:53 +0100

    Update configfile_to_repo.sh

--- configfile_to_repo.sh
+++ configfile_to_repo.sh
@@ -8,11 +8,3 @@ sudo mv /etc/hostapd/hostapd.conf /home/pi/raspbianrouter/hostapd.conf
 sudo mv /etc/sysctl.conf /home/pi/raspbianrouter/sysctl.conf
 sudo mv /proc/sys/net/ipv4/ip_forward /home/pi/raspbianrouter/ip_forward
 
-# Symlink config files to original location
-sudo ln -s /home/pi/raspbianrouter/interfaces /etc/network/interfaces 
-sudo ln -s /home/pi/raspbianrouter/dhcpd.conf /etc/dhcp/dhcpd.conf 
-sudo ln -s /home/pi/raspbianrouter/isc-dhcp-server /etc/default/isc-dhcp-server 
-sudo ln -s /home/pi/raspbianrouter/hostapd.conf /etc/hostapd/hostapd.conf 
-sudo ln -s /home/pi/raspbianrouter/sysctl.conf /etc/sysctl.conf 
-sudo ln -s /home/pi/raspbianrouter/ip_forward /proc/sys/net/ipv4/ip_forward 
-
